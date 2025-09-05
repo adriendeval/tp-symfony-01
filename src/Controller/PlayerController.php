9c4fb2e50f5060a706f93258ce43f652e3cfe6b1
deval <deval@sio.ndlp>
2025-09-05 14:57:02 +0200

    ad deletion and update (to fix)

--- src/Controller/PlayerController.php
+++ src/Controller/PlayerController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Player;
 use App\Repository\PlayerRepository;
+use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,6 +12,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PlayerController extends AbstractController
 {
+    private PlayerRepository $playerRepository;
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(PlayerRepository $playerRepository, EntityManagerInterface $entityManager)
+    {
+        $this->playerRepository = $playerRepository;
+        $this->entityManager = $entityManager;
+    }
 
     #[Route('/player/', name: 'app_player')]
     public function index(PlayerRepository $playerRepository): Response
@@ -23,14 +32,39 @@ class PlayerController extends AbstractController
     }
 
     #[Route('/player/create', name: 'create_player')]
-    public function create(EntityManagerInterface $entityManager): Response
+    public function create(): Response
     {
         $player = new Player();
-        $player->setName('Miss Fortune');
+        $player->setName('Tata');
+        $player->setXp(100);
 
-        $entityManager->persist($player);
-        $entityManager->flush();
+        $this->entityManager->persist($player);
+        $this->entityManager->flush();
 
         return new Response('Joueur créé avec l\'ID '.$player->getId());
+    }
+
+    #[Route('/player/update/{id}', name: 'update_player')]
+    public function update(int $id): Response
+    {
+        $player = $this->playerRepository->find($id);
+
+        $player->setName('Mario');
+        $this->playerRepository->flush();
+
+        return new Response('Le joueur {id} a bien été mis à jour.');
+    }
+
+    #[Route('/player/delete/{id}', name: 'delete_player')]
+    public function delete(int $id): Response
+    {
+        $player = $this->playerRepository->find($id);
+
+        if ($player) {
+            $this->entityManager->remove($player);
+            $this->entityManager->flush();
+        }
+
+        return new Response('Le joueur {id} a bien été supprimé.');
     }
 }
