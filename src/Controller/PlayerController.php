972ef701eff203c29413cadc353071f34d733b75
deval <deval@sio.ndlp>
2025-09-05 14:16:10 +0200

    add playerRepository

--- src/Controller/PlayerController.php
+++ src/Controller/PlayerController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Player;
+use App\Repository\PlayerRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,6 +11,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PlayerController extends AbstractController
 {
+
+    #[Route('/player/', name: 'app_player')]
+    public function index(PlayerRepository $playerRepository): Response
+    {
+        $players = $playerRepository->findAll();
+
+        return $this->render('player/index.html.twig', [
+            'players' => $players,
+        ]);
+    }
+
     #[Route('/player/create', name: 'create_player')]
     public function create(EntityManagerInterface $entityManager): Response
     {
@@ -19,6 +31,6 @@ class PlayerController extends AbstractController
         $entityManager->persist($player);
         $entityManager->flush();
 
-        return new Response('Player created with id '.$player->getId());
+        return new Response('Joueur créé avec l\'ID '.$player->getId());
     }
 }
